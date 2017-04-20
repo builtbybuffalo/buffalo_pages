9207c8572fcd1270b4accb06112805cb05f95abb
jaspertandy <jspr@tndy.me>
2017-04-20 15:06:18 +0100

    Support loading and modifying locale files

--- app/models/site.rb
+++ app/models/site.rb
@@ -12,8 +12,78 @@ class Site < ActiveRecord::Base
   belongs_to :footer_menu, class_name: "Menu", foreign_key: :footer_menu_id
   belongs_to :legal_menu, class_name: "Menu", foreign_key: :legal_menu_id
 
+  attr_accessor :locale_vars
+
   def locale
-    self[:locale].presence || self.name.downcase
+    self[:locale].presence || self.name.to_s.downcase
+  end
+
+  def locale_root
+    "#{Rails.root}/config/locales/"
+  end
+
+  def copy_default_locales
+    default = Site.where.not(id: id).find_by(default: true)
+    if default.present?
+      default.locale_paths.each do |path|
+        target_path = path.sub(/#{default.locale}.yml$/, "#{locale}.yml")
+        default_contents = YAML.load_file(path)
+        converted_contents = {}
+        converted_contents[locale] = default_contents[default.locale]
+        File.open(target_path, "w") do |f|
+          f.write(converted_contents.to_yaml)
+          f.close
+        end
+      end
+    end
+  end
+
+  def core_locale_path
+    paths = []
+    core = "#{locale_root}#{locale}.yml"
+    paths.push(core) if File.exists?(core)
+
+    paths
+  end
+
+  def full_locale_paths
+    paths = core_locale_path
+
+    if paths.present?
+      paths + Dir.glob("#{locale_root}*.#{locale}.yml")
+    else
+      []
+    end
+  end
+
+  def locale_paths
+    if core_locale_path.empty? && locale.present?
+      copy_default_locales
+    end
+
+    full_locale_paths
+  end
+
+  def locale_vars
+    locale_paths.map do |path|
+      vars = YAML.load_file(path)
+
+      [path.split("/").last.split(".")[0..-2].join("."), vars]
+    end.to_h
+  end
+
+  def locale_vars=(vars)
+    vars.each do |file, vars|
+      filepath = "#{locale_root}#{file}.yml"
+      backuppath = "#{locale_root}#{file}--#{DateTime.current.utc.to_s.parameterize.underscore}.backup"
+      FileUtils.cp(filepath, backuppath)
+      File.open(filepath, "w") do |f|
+        f.write(JSON.parse(vars.to_json).to_yaml)
+        f.close
+      end
+    end
+
+    I18n.backend.reload!
   end
 
   class << self
